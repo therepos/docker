807580aff2013cad9bafd986b21673b31344a73f
The Repos <ry4nyeo@gmail.com>
2025-03-17 20:08:55 +0800

    updates

--- pdfai-b/extract.py
+++ pdfai-b/extract.py
@@ -17,7 +17,8 @@ def extract_text_from_epub(epub_path):
 
 def extract_text_from_mobi(mobi_path):
     """Extract text from a MOBI file."""
-    return extract(mobi_path)
+    extracted_text = mobi.extract(mobi_path)
+    return extracted_tex
 
 def extract_text_from_pdf(pdf_path):
     """Extract text from a digital/text-based PDF."""
